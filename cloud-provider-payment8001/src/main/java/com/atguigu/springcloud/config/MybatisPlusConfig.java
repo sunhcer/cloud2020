8dde6e8f8e09afb87bc074b3d18688cc5b27605f
sunhcer <1107500755@qq.com>
2021-05-30 17:47:01 +0800

    mybatisplus 集成 分页 ,增加分页配置类,yml增加配置,修改pagehelper版本

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/config/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package com.atguigu.springcloud.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.github.pagehelper.PageInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * plus分页配置
+ * @author sunhcer
+ * @date 2021/05/30 17:38
+ **/
+@Configuration
+public class MybatisPlusConfig {
+    /*
+     * 分页插件，自动识别数据库类型
+     * 多租户，请参考官网【插件扩展】
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    @Bean
+    PageInterceptor pageInterceptor(){
+        PageInterceptor pageInterceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        properties.setProperty("helperDialect", "mysql");
+        pageInterceptor.setProperties(properties);  // 由此可进入源码，
+        return pageInterceptor;
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/MybtisController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/MybtisController.java
@@ -3,8 +3,13 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.annotation.ControllerWebLog;
 import com.atguigu.springcloud.annotation.MemoryCaculateLog;
 import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.User;
 import com.atguigu.springcloud.service.ICompanyService;
+import com.atguigu.springcloud.service.UserService;
 import com.atguigu.springcloud.vo.Company;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -31,6 +36,8 @@ public class MybtisController {
     @Qualifier
     private ICompanyService iCompanyService;
 
+    @Autowired
+    private UserService userService;
 
     @GetMapping("payment/selectTwoLevel/{id}")
     @ControllerWebLog(name="测试2层嵌套")
@@ -70,4 +77,21 @@ public class MybtisController {
         }
         return CommonResult.succ("成功");
     }
+
+
+    /**
+     * 和mybatisplus冲突,需要加配置类paginationInterceptor
+     * @param pageSize
+     * @param pageNum
+     * @return
+     */
+    @GetMapping("payment/page")
+    @MemoryCaculateLog
+    public CommonResult getpage(@RequestParam int pageSize,@RequestParam int pageNum){
+        PageHelper.startPage(pageNum,pageSize);
+        List<User> list = userService.list(new QueryWrapper<User>().lambda().ne(User::getId,1111L));
+        PageInfo<User> userPageInfo = new PageInfo<>(list);
+        return CommonResult.succ(userPageInfo);
+    }
+
 }
